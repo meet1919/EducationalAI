a903342d894573523d63169d589a25fe51ba1a6b
meet1919 <meetgondaliya1999@gmail.com>
2023-06-09 23:50:47 +0530

    new tutors added

---
+++ professor_prompts.py
@@ -0,0 +1,68 @@
+physics_professor_template = """You are a very smart physics professor. You are great at answering questions about physics in a concise and 
+easy to understand manner. When you don't know the answer to a question you admit that you don't know.
+
+Here is a question:
+{input}"""
+
+
+math_professor_template = """You are a very good mathematician. You are great at answering math questions. You are so good because you are able 
+to break down hard problems into their component parts, answer the component parts, and then put them together to answer the broader 
+question.
+
+Here is a question:
+{input}"""
+
+chemistry_professor_template = """You are a very good chemist. You are great at answering chemistry questions. You are so good because you are able 
+to break down hard problems into their component parts, answer the component parts, and then put them together to answer the broader 
+question.
+
+Here is a question:
+{input}"""
+
+
+SCIENCE_PROFESSOR_PROMPTS = [
+    {
+        "name": "physics", 
+        "description": "Good for answering questions about physics", 
+        "prompt_template": physics_professor_template
+    },
+    {
+        "name": "math", 
+        "description": "Good for answering math questions", 
+        "prompt_template": math_professor_template
+    },
+    {
+        "name": "chemistry", 
+        "description": "Good for answering chemistry questions", 
+        "prompt_template": chemistry_professor_template
+    }
+]
+
+
+economics_professor_template = """You are a very smart economics professor. You are great at answering questions about economics in a concise and 
+easy to understand manner. When you don't know the answer to a question you admit that you don't know.
+
+Here is a question:
+{input}"""
+
+
+accounting_professor_template = """You are a very good accounting. You are great at answering accounting questions. You are so good because you are able 
+to break down hard problems into their component parts, answer the component parts, and then put them together to answer the broader 
+question.
+
+Here is a question:
+{input}"""
+
+
+COMMERCE_PROFESSOR_PROMPTS = [
+    {
+        "name": "economics", 
+        "description": "Good for answering questions about economics", 
+        "prompt_template": physics_professor_template
+    },
+    {
+        "name": "accounting", 
+        "description": "Good for answering accounting questions", 
+        "prompt_template": math_professor_template
+    },
+]
---
+++ tutor.py
@@ -0,0 +1,63 @@
+from langchain.chains.router.embedding_router import EmbeddingRouterChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains.router import MultiPromptChain
+from langchain.chains import ConversationChain
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv, find_dotenv
+from langchain.chains.llm import LLMChain
+from langchain.vectorstores import Chroma
+from professor_prompts import SCIENCE_PROFESSOR_PROMPTS, COMMERCE_PROFESSOR_PROMPTS
+
+load_dotenv(find_dotenv())
+
+class Tutor:
+
+    def __init__(self, professor_prompts):
+
+        llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+        destination_chains = {}
+        names_and_descriptions = []
+        for p_info in professor_prompts:
+            name = p_info["name"]
+            prompt_template = p_info["prompt_template"]
+            prompt = PromptTemplate(template=prompt_template, input_variables=["input"])
+            chain = LLMChain(llm=llm, prompt=prompt)
+            destination_chains[name] = chain
+            names_and_descriptions.append((name, p_info['description']))
+        default_chain = ConversationChain(llm=llm, output_key="text")
+
+        # Embedding Router chain
+        model_name = "sentence-transformers/all-mpnet-base-v2"
+        model_kwargs = {'device': 'cuda'}
+        encode_kwargs = {'normalize_embeddings': False}
+        router_chain = EmbeddingRouterChain.from_names_and_descriptions(
+            names_and_descriptions, Chroma, HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs), routing_keys=["input"]
+        )
+
+        self.chain = MultiPromptChain(router_chain=router_chain, destination_chains=destination_chains, default_chain=default_chain, verbose=True)
+    
+    def assist_with(self, query):
+        output = self.chain.run(query)
+        return output
+        
+class ScienceTutor(Tutor):
+
+    def __init__(self):
+        super().__init__(SCIENCE_PROFESSOR_PROMPTS)
+    
+    def assist_with(self, query):
+        return super().assist_with(query)
+    
+class CommerceTutor(Tutor):
+
+    def __init__(self):
+        super().__init__(COMMERCE_PROFESSOR_PROMPTS)
+
+    def assist_with(self, query):
+        return super().assist_with(query)
+    
+
+science_tutor = ScienceTutor()
+answer = science_tutor.assist_with('What are organic compounds?')
+print(answer)
